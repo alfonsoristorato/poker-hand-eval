0d70313df1ae282b4780fc1eb28b6904c604a7a1
alfonsoristoratoSky <alfonso.ristorato@sky.uk>
2022-08-26 14:48:09 +0100

    added royal flash rpivate check

--- src/main/java/com/poker/alfonso/App.java
+++ src/main/java/com/poker/alfonso/App.java
@@ -5,14 +5,24 @@ package com.poker.alfonso;
 public class App {
     public static void main(String[] args) {
         Deck deck = new Deck();
+        System.out.println("/////////////////////////////");
         System.out.println("First card in deck: " + deck.deck.get(0).stringify(deck.deck.get(0)));
+        System.out.println("/////////////////////////////");
 
         deck.shuffle(deck);
+        System.out.println("/////////////////////////////");
         System.out.println("First card in deck after shuffle: " + deck.deck.get(0).stringify(deck.deck.get(0)));
+        System.out.println("/////////////////////////////");
 
+        System.out.println("/////////////////////////////");
         System.out.println("Deck size: " + deck.deck.size());
+        System.out.println("/////////////////////////////");
         Hand hand = new Hand(deck);
+        System.out.println("/////////////////////////////");
         System.out.println("Deck size after creating hand: " + deck.deck.size());
+        System.out.println("/////////////////////////////");
+        System.out.println("/////////////////////////////");
         System.out.println("Hand elements: " + hand.stringifiedHand);
+        System.out.println("/////////////////////////////");
     }
 }
--- src/main/java/com/poker/alfonso/Hand.java
+++ src/main/java/com/poker/alfonso/Hand.java
@@ -1,7 +1,10 @@
 package com.poker.alfonso;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class Hand {
     List<Card> hand;
@@ -13,13 +16,28 @@ public class Hand {
             stringifiedHand.append(card.stringify(card) + "; ");
         });
         deck.deck.subList(0, 5).clear();
-        
+
     }
 
     public List<Card> getHand() {
         return hand;
     }
 
-   
+    private boolean royalFlush() {
+        if (hand.stream().allMatch((card) -> card.getSuit() == hand.get(0).getSuit())) {
+            Integer[] royalFlush = { 0, 9, 10, 11, 12 };
+            Integer[] suitsFromHand = hand.stream().map(Card::getRank).collect(Collectors.toList())
+                    .toArray(new Integer[0]);
+
+            if (royalFlush.equals(suitsFromHand)) {
+                return true;
+            } else {
+                return false;
+            }
+
+        } else {
+            return false;
+        }
+    }
 
 }
